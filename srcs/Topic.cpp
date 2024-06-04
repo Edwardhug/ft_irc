1a16c1ffa6a272673695afa0d6378c455b4ed1cb
Mac de Lucien <luciengabet0@gmail.com>
2024-06-04 16:29:13 +0200

    need to fix some TOPIC issues

--- srcs/Topic.cpp
+++ srcs/Topic.cpp
@@ -13,14 +13,16 @@ void Server::splitForTopic(std::string buff, int fdSender) {
 			return;
 		}
 		channel.setTopic(data[2]);
-		// std::string message = "332 " + client.getNick() + " " + channel.getName() + " :" + channel.getTopic() + "\r\n";
-		std::string message = ":" + client.getNick() + " TOPIC " + channel.getName() + " " + channel.getTopic() + "\r\n";
+		std::string channelName = channel.getName();
+		channelName.resize(channelName.size() - 1);
+		std::string message = ":" + client.getNick() + " TOPIC " + channelName + " " + channel.getTopic() + "\r\n";
 		servSendMessageToClient(message, client);
 	}
 	else if (data.size() == 2 && channelExist(data[1]) == true) {
 		Channel &channel = findChannelWithName(data[1]);
-		std::string message = ":" + client.getNick() + " TOPIC " + channel.getName() + " " + channel.getTopic() + "\r\n";
-		// std::string message = "332 " + client.getNick() + " " + channel.getName() + " :" + channel.getTopic() + "\r\n";
+		std::string channelName = channel.getName();
+		channelName.resize(channelName.size() - 1);
+		std::string message = ":" + client.getNick() + " TOPIC " + channelName + " " + channel.getTopic() + "\r\n";
 		servSendMessageToClient(message, client);
 	}
 	else {
